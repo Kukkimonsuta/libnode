6ba2d990186db5dfb6450a3cf7fce0436856863f
Lukáš Novotný <lukas@dramiel.com>
2021-11-21 19:07:23 +0100

    downgrade python; remove smallicu

--- scripts/config.py
+++ scripts/config.py
@@ -6,7 +6,3 @@ nodeVersion = os.environ['LIBNODE_NODE_VERSION']
 configFlags = (os.environ.get('LIBNODE_CONFIG_FLAGS') or '').split()
 x86 = os.environ.get('LIBNODE_X86') == '1'
 zipBasenameSuffix = os.environ.get('LIBNODE_ZIP_SUFFIX', '')
-
-if os.environ.get('LIBNODE_SMALL_ICU', '') == '1':
-	configFlags += ['--with-intl=small-icu']
-	zipBasenameSuffix += '-smallicu'
